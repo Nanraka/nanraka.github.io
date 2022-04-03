d10a537da81d9a264e6aaf8ad6786fb0e9c69098
Nanraka <64336110+Initiatechy@users.noreply.github.com>
2022-04-03 15:28:46 +0900

    use vue ver

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+   <BootstrapTest />
+ </div>
+</template>
+
+<script>
+import BootstrapTest from './components/modules/WebButton.vue'
+ 
+export default {
+ name: 'App',
+ components: {
+   BootstrapTest
+ }
+}
+</script>
+
+<style>
+#app {
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+ margin-top: 60px;
+}
+</style>

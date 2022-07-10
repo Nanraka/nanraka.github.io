a49814eeb8cb165063f967a9355d015318e3bd47
Nanraka <64336110+Initiatechy@users.noreply.github.com>
2022-07-10 14:17:24 +0900

    cahnge html

--- src/components/modules/MainImg.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div id = "main visual">
-        <img src = "./img/test.jpg" class="img-fluid" alt="趣味の電子工作ブログ">
-    </div>
-</template>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '../router.js' // Routerの使用
-import "bootstrap/dist/css/bootstrap.min.css"
-
-// 各javascriptファイルの使用
-createApp(App).use(router).mount('#app')
